9dbafaf448e5c976ccd93d3a4a8b9b4e56574919
guilermosantos <gsantos@webimpacto.es>
2020-05-13 16:24:59 +0200

    Hecho formulario

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,7 @@
 
 namespace App\DataFixtures;
 
-// use App\Emtity\User;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -13,7 +13,7 @@ class AppFixtures extends Fixture
         // $user = new User();
         // $user->setEmail('gsantos@web-impacto.es');
         // $user->setRoles(['ROLE_USER']);
-        // $user->setPassword('guillermo');
+        // $user->setPassword('$argon2id$v=19$m=65536,t=4,p=1$STdSdEhjYWp5M1BsTUdPTg$c5S6BGv/sdbTuCAA13IIpev8k3LnbBB6smOmsiFOhF4');
         // $manager->persist($user);
         // $manager->flush();
     }
