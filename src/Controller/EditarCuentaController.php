d5d2f0318c06c90c7e77135647b571eefce8243e
guilermosantos <gsantos@webimpacto.es>
2020-05-14 17:48:11 +0200

    Acabando página mi-cuenta. Falta poder editar país y provincia

--- src/Controller/EditarCuentaController.php
+++ src/Controller/EditarCuentaController.php
@@ -5,6 +5,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\Constraints\DateTime;
 class EditarCuentaController extends AbstractController
 {
     /** 
@@ -19,29 +21,30 @@ class EditarCuentaController extends AbstractController
     /** 
     * @Route("/mi-cuenta/editarCuenta") 
     */ 
-    public function editar(){
-
+    public function editar(UserPasswordEncoderInterface $passwordEncoder){
+        
+        //dump("Llega a editar en el controller"); 
         $entityManager = $this->getDoctrine()->getManager();
-
         $user = $this->get('security.token_storage')->getToken()->getUser();
-        dump($user);
-        dump("Llega a editar en el controller");
+        $passwordForm = $_REQUEST["password"];
+        if($passwordForm!=""){
+            $password = $passwordEncoder->encodePassword($user,$passwordForm);
+            $user->setPassword($password);
+        }
         $user->setNombre($_REQUEST["nombre"]);
         $user->setApellidos($_REQUEST["apellidos"]);
         $user->setUsername($_REQUEST["email"]);
-        //$user->setPassword($_REQUEST["password"]);
-        //$user->setFechadenacimiento($_REQUEST["fechaNacimiento"]);
-        dump($_REQUEST["fechaNacimiento"]);
-        // dump($user->getFechadenacimiento());
+        //dump($_REQUEST["fechaNacimiento"]);
+        $date = date_create_from_format('Y-m-d', $_REQUEST["fechaNacimiento"]);
+        $user->setFechadenacimiento($date);
+        //dump($user->getFechadenacimiento());
         $user->setSexo($_REQUEST["sexo"]);
         $user->setTelefono($_REQUEST["telefono"]);
         $user->setDatosadicionales($_REQUEST["datosAdicionales"]);
         $user->setCiudad($_REQUEST["ciudad"]);
-        dump($_REQUEST["direccion"]);
         $user->setDireccion($_REQUEST["direccion"]);
         $user->setCodigopostal($_REQUEST["codigoPostal"]);
-        dump($user);
-        
+        //dump($user);
         $entityManager->flush();
         return $this->render('usuario/mi-cuenta.html.twig');
     }
